c690e109d8ea814f3e6a83089bbd115a6de8e13c
aki <heti@qq.com>
2019-11-10 12:53:30 +0800

    '0.0.17'

--- src/akitools.py
+++ src/akitools.py
@@ -1,18 +1,18 @@
 # Copyright (c) 2019 aki
 
 
-__all__ = ['version', 'ftime', 'ftimestamp', 'header', 'HEADER', 'send_mail', 'log_write']
+__all__ = ['version', 'header', 'ftime', 'ctime', 'send_mail', 'log_write']
 
 
-version = '0.0.16'
+version = '0.0.17'
 
-
-HEADER = header()
+header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'}
 
 
 def ftime(f: int = None, t: int = None, c: str = None) -> str:
     """
-    将时间戳转换成指定的日期/时间格式
+    将时间戳转换成日期/时间字符串
+
         f: 已知的格式               # 为空则默认为 1 ,返回格式为: 20140320
         t: 时间戳数字               # 为空则默认为 当前时间的时间戳
         c: 自定义格式               # 参考 '%Y%m%d' 格式
@@ -45,17 +45,18 @@ def ftime(f: int = None, t: int = None, c: str = None) -> str:
     return time.strftime(KNOWN_FORMATS.get(f, KNOWN_FORMATS[1]), time.localtime(t))
 
 
-def ftimestamp(d: str, f: int = None, c: str = None) -> int:
+def ctime(d: str = None, f: int = None, c: str = None) -> int:
     """
-    将一个日期/时间转换成时间戳
-        d:  日期/时间               # 2014-03-20
+    将日期/时间字符串转换成时间戳
+
+        d:  日期/时间               # 20140320
         f:  已知格式                # 为空则默认为 1
         c:  自定义格式              # 参考 '%Y%m%d' 格式
         如果参数f 与 c 都非空，则优先选择参数c 自定义的时间格式
             f = 1   =  '%Y%m%d'
             f = 2   =  '%Y.%m.%d'
-            f = 3   =  2014-03-20
-            f = 4   =  '%Y-%m-%d'
+            f = 3   =  '%Y-%m-%d'
+            f = 4   =  '%Y/%m/%d'
             f = 5   =  '%Y%m%d %H%M%S'
             f = 6   =  '%Y.%m.%d %H:%M:%S'
             f = 7   =  '%Y-%m-%d %H:%M:%S'
@@ -74,23 +75,9 @@ def ftimestamp(d: str, f: int = None, c: str = None) -> int:
         8: '%Y/%m/%d %H:%M:%S',
     }
     f = c if c else KNOWN_FORMATS.get(f, KNOWN_FORMATS[1])
-    return int(time.mktime(time.strptime(d, f)))
-
-
-def header() -> dict:
-    """
-    随机返回一个User-Agent,格式为字典
-        {'User-Agent':'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Maxthon 2.0)'}
-    """
-    from random import choice
-
-    KNOWN_LIST = ['Mozilla/5.0 (Windows; U; Windows NT 6.1; rv:2.2) Gecko/20110201',
-                  'Opera/9.80 (Windows NT 6.0) Presto/2.12.388 Version/12.14',
-                  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/7046A194A',
-                  'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:64.0) Gecko/20100101 Firefox/64.0',
-                  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36',
-                  ]
-    return {'User-Agent': choice(KNOWN_LIST)}
+    if d:
+        return int(time.mktime(time.strptime(d, f)))
+    return int(time.time())
 
 
 def send_mail(recipient: list, subject: str, text: str):
@@ -105,8 +92,6 @@ def send_mail(recipient: list, subject: str, text: str):
     import smtplib
 
     message = MIMEText(text, 'plain', 'utf-8')
-    # message['From'] = None
-    # message['To'] = None
     message['Subject'] = Header(subject, 'utf-8')
     try:
         smtpObj = smtplib.SMTP('localhost')
